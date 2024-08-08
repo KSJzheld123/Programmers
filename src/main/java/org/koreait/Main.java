c4ebde86027f6d6793fd667a88f1235138b3b190
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-08-08 16:46:59 +0900

    d20240808

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,18 +1,30 @@
 class Solution {
-    public long solution(String numbers) {
-        long answer = 0;
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        int count = 0;
+        String[] spell2 = new String[spell.length];
 
-        numbers = numbers.replaceAll("zero", "0");
-        numbers = numbers.replaceAll("one", "1");
-        numbers = numbers.replaceAll("two", "2");
-        numbers = numbers.replaceAll("three", "3");
-        numbers = numbers.replaceAll("four", "4");
-        numbers = numbers.replaceAll("five", "5");
-        numbers = numbers.replaceAll("six", "6");
-        numbers = numbers.replaceAll("seven", "7");
-        numbers = numbers.replaceAll("eight", "8");
-        numbers = numbers.replaceAll("nine", "9");
-        answer = Long.parseLong(numbers);
+        for(int i = 0; i < spell.length; i++) {
+            spell2[i] = spell[i];
+        }
+
+        for (int i = 0; i < dic.length; i++) {
+            for(int j = 0; j < spell.length; j++) {
+                if(dic[i].contains(spell[j])) {
+                    spell[j] = "-";
+                    count++;
+                    break;
+                }
+            }
+
+            for(int j = 0; j < spell2.length; j++) {
+                spell[j] = spell2[j];
+            }
+        }
+
+        if(count == spell.length) {
+            answer = 1;
+        }
 
         return answer;
     }
